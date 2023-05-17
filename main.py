18414960d65d4a647be03c99c8cfb2e2ccad3fa9
DFori <daniel.tatama@binghamuni.edu.ng>
2023-05-17 12:12:04 +0100

    saved entries

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from tkinter import *
+from tkinter import messagebox
 BLUE = "#97DEFF"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = web_ent.get()
+    email = name_ent.get()
+    password = password_ent.get()
+
+    messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                  f"\nPassword: {password} \nIs it ok to save? ")
+
+    with open("data.txt", 'a') as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        web_ent.delete(0, END)
+        password_ent.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -16,7 +30,7 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
 
-website_lb = Label(text="website:", fg="white", bg="black", font=("Arial", 16))
+website_lb = Label(text="Website:", fg="white", bg="black", font=("Arial", 16))
 website_lb.grid(column=0, row=1)
 
 web_ent = Entry(width=35, bg="black", highlightthickness=0)
@@ -27,6 +41,7 @@ name_lb = Label(text="Email/Username:", fg="white", bg="black", font=("Arial", 1
 name_lb.grid(column=0, row=2)
 
 name_ent = Entry(width=35, bg="black", highlightthickness=0)
+name_ent.insert(0, "danielfori04@gmail.com")
 name_ent.grid(column=1, row=2, columnspan=2)
 
 password_lb = Label(text="Password:", fg="white", bg="black", font=("Arial", 16))
@@ -39,7 +54,7 @@ password_btn = Button(text="Generate Password", highlightthickness=0, highlightb
 password_btn.grid(column=2, row=3)
 
 
-add_btn = Button(text="Add", highlightthickness=0, highlightbackground="black", width=30)
+add_btn = Button(text="Add", highlightthickness=0, highlightbackground="black", width=30, command=save)
 add_btn.grid(column=1, row=4, columnspan=2)
 
 
